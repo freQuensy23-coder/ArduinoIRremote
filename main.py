f7e1dca22b4d4de35bc0a3238fdfe516feba0070
freQuensy <mametevalex@gmail.com>
2020-05-26 17:15:07 +0300

    Добавлена ветка сеня

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ def read_from_json(filename):
         result = json.load(f)
         return result
 
+#TODO: (Cert) Начать делать дизайн для каждой кнопки оставить свободным выбор функции. Все функции пиши одним блоком. Делай ООП только если знаешь что делаешь
+#TODO: (Alex) реализовать ввод данных с Serial порта
 
 def main():
-    keys = read_from_json("config.json")
+    keys = read_from_json("config.json") #Считываем номера клавиш из дсон хранилища
+                                         #TODO: (Alex) Добавить в дсон все клавиши как в картикне
 
+    print(keys)
 
 if __name__ == '__main__':
     main()
